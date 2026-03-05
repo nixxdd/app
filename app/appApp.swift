267f509a3b38b98c4d2b0964ba05cf3ba6f29596
Stefania <stefaniarosci2007@gmail.com>
2026-03-05 09:30:15 +0100

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Foundation 34 on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
